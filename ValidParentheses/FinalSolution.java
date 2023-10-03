57ae3bb4db44d0571f15466f1348b9e9c5561897
Vinícius Stein Dahmer <30452636+viniciussdahmer@users.noreply.github.com>
2023-10-02 21:07:08 -0300

    feat(ValidParentheses): using a single data structure to fix missing test cases

---
+++ ValidParentheses/FinalSolution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push('(');
+            } else if (c == '[') {
+                stack.push('[');
+            } else if (c == '{') {
+                stack.push('{');
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } 
+        }
+        
+        return stack.isEmpty() ? true : false;
+    }
+}
